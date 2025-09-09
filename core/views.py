0d62e892f114b35dae30f36200dda59e86d5b881
renan melo <renan.melo@conjel.com.br>
2025-09-09 10:36:34 -0300

    ajustes de Banco de dados

--- core/views.py
+++ core/views.py
@@ -235,16 +235,3 @@ def minhas_ideias_visiveis(request):
         "eh_gestor": is_gestor(user)
     })
 
-from django.contrib.auth import get_user_model
-from django.http import HttpResponse
-
-def create_admin(request):
-    User = get_user_model()
-    username = "admin"
-    password = "Re301203@"
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email="", password=password)
-        return HttpResponse("Superusuário criado com sucesso!")
-    return HttpResponse("Superusuário já existe.")
-
